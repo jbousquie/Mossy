9f540150bd176f1b84c99c03e48346d5e3522bcf
jerome <jerome.bousquie@rodez.fr>
2023-11-16 16:29:09 +0100

    tilesource modif

--- assets/images/Mossy Assets/Mossy Tileset/montage.sh
+++ assets/images/Mossy Assets/Mossy Tileset/montage.sh
@@ -1,1 +1,1 @@
-montage Mossy\ -\ TileSet4k.png Mossy\ -\ BackgroundDecoration.png Mossy\ -\ Decorations\&Hazards.png PlantsPlatforms4k.png  Mossy\ -\ MossyHills4k.png -geometry 1024x1024 -tile 2x3  -background none mossy-global128.png
+montage Mossy\ -\ TileSet4k.png Mossy\ -\ BackgroundDecoration.png  -geometry 1024x1024  -background none mossy-global128.png
