4eb1f59c44576e60c0bf6aa321bb35a5b5397f6a
Jérôme Bousquié <jerome.bousquie@gmail.com>
2023-11-20 16:31:36 +0100

    slimes

---
+++ main/enemies/factory.go
@@ -0,0 +1,53 @@
+components {
+  id: "factory"
+  component: "/main/enemies/slime_factory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "slimegreen_factory"
+  type: "factory"
+  data: "prototype: \"/main/enemies/slimegreen.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "slimeorange_factory"
+  type: "factory"
+  data: "prototype: \"/main/enemies/slimeorange.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
